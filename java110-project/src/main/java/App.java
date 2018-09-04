fb62e8db1c07f4944a6a23454579e3e7290516e4
JeahaOh <jeaha1217@gmail.com>
2018-09-04 14:57:04 +0900

    메소드 적용하여 코드 분리

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        
-        String[] names = new String[100];
-        String[] emails = new String[100];
-        String[] passwords = new String[100];
-        
-        int index = 0;
-        
-        // 1) 키보드 입력을 처리할 객체 준비
-        Scanner keyIn = new Scanner(System.in);
-        
-        // 2) 사용자로부터 회원 정보 입력 받기
-        
+    static String[] names = new String[100];
+    static String[] emails = new String[100];
+    static String[] passwords = new String[100];
+
+    static int index = 0;
+
+    static Scanner keyIn = new Scanner(System.in);
+
+    static void inputMembers() {
+        // 
         while (true) {
             System.out.print("이름? ");
             names[index] = keyIn.nextLine();
@@ -31,12 +28,19 @@ public class App {
             if (answer.toLowerCase().equals("n"))
                 break;
         }
-        
-        
+    }   //  printMembers
+    
+    static void printMembers() {
         for(int i = 0; i < index; i++) {
             System.out.printf("%s, %s, %s\n", names[i], emails[i], passwords[i]);
-            
         }
+    }   //  inputMembers
+    
+    public static void main(String[] args) {
+        
+        inputMembers();
+        
+        printMembers();
         
         keyIn.close();
         
