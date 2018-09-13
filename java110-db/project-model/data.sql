b37b8f4519ebad9fa356eb9ad461d4b534550ca4
JeahaOh <jeaha1217@gmail.com>
2018-09-13 14:35:26 +0900

    .

--- java110-db/project-model/data.sql
+++ java110-db/project-model/data.sql
@@ -27,3 +27,20 @@ values(9, 'user09', 'user09@test.com',  password('1111'), now());
 
 insert into p1_memb(mno, name, email, pwd, cdt)
 values(10, 'user10', 'user10@test.com',  password('1111'), now());
+
+
+학생회원
+insert into p1_stud(sno, schl, work)values(1, 'Bit Unv.', 'N');
+insert into p1_stud(sno, schl, work)values(2, 'BitCamp Unv.', 'Y');
+insert into p1_stud(sno, schl, work)values(3, 'BitCamp Unv.', 'N');
+insert into p1_stud(sno, schl, work)values(4, 'Bit Unv.', 'Y');
+insert into p1_stud(sno, schl, work)values(5, '오호라 UNV.', 'Y');
+
+강사 회원
+insert into p1_tchr(tno, hrpay, subj)values(6, 10000, 'JAVA,C,C++');
+insert into p1_tchr(tno, hrpay, subj)values(7, 12000, 'JAVA,BigData');
+insert into p1_tchr(tno, hrpay, subj)values(8, 14000, 'C,C++,Adroid');
+
+매니저 회원
+insert into p1_mgr(mrno, posi)values(9, '대리');
+insert into p1_mgr(mrno, posi)values(10, '과장');
