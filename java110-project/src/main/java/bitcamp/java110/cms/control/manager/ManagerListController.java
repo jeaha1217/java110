24e8652cd28ae5a403f99df12f83b36743593b67
JeahaOh <jeaha1217@gmail.com>
2018-09-13 18:04:36 +0900

    Manager CRUD JDBC 작성

--- java110-project/src/main/java/bitcamp/java110/cms/control/manager/ManagerListController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/manager/ManagerListController.java
@@ -24,7 +24,8 @@ public class ManagerListController {
         List<Manager> list = managerDao.findAll();
         
         for (Manager m : list) {
-            System.out.printf("%s, %s, %s, %s, %s\n", 
+            System.out.printf("%d %s, %s, %s, %s, %s\n", 
+                    m.getNo(),
                     m.getName(), 
                     m.getEmail(), 
                     m.getPassword(), 
