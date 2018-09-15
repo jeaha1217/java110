0b8392e5c92a923628a4e6d8baf3fa279f2ab62b
JeahaOh <jeaha1217@gmail.com>
2018-09-15 17:45:36 +0900

    ClientApp, ServerApp분리 후, XXXX/list 요청 처리

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp2.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp2.java
@@ -1,5 +1,4 @@
 package bitcamp.java110.cms.server;
-
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,47 +11,49 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import bitcamp.java110.cms.context.RequestMappingHandlerMapping;
 import bitcamp.java110.cms.context.RequestMappingHandlerMapping.RequestMappingHandler;
 
-public class ServerApp {
-    
+public class ServerApp2 {
     ClassPathXmlApplicationContext iocContainer;
     RequestMappingHandlerMapping requestHandlerMap;
     
-    public ServerApp() throws Exception {
-        createIoCContainer();
+    public ServerApp2() throws Exception{
+        createIocContainer();
         logBeansOfContainer();
         processRequestMappingAnnotation();
     }
-    
-    private void createIoCContainer() {
-        iocContainer = new ClassPathXmlApplicationContext(
-                "bitcamp/java110/cms/conf/application-context.xml");
+
+    private void createIocContainer() {
+        iocContainer = 
+                new ClassPathXmlApplicationContext
+                ("bitcamp/java110/cms/conf/application-context.xml");
     }
     
     private void processRequestMappingAnnotation() {
-        requestHandlerMap = new RequestMappingHandlerMapping();
+        requestHandlerMap = 
+                new RequestMappingHandlerMapping();
+        
         String[] names = iocContainer.getBeanDefinitionNames();
         for (String name : names) {
             Object obj = iocContainer.getBean(name);
+            
             requestHandlerMap.addMapping(obj);
         }
     }
     
     private void logBeansOfContainer() {
-        System.out.println("------------------------");
-        String[] nameList = iocContainer.getBeanDefinitionNames();
-        for (String name : nameList) {
+        System.out.println("--------------------------");
+        String[] namelist = iocContainer.getBeanDefinitionNames(); 
+        for(String name : namelist) {
             System.out.println(name);
         }
-        System.out.println("------------------------");
+        System.out.println("--------------------------");
     }
     
-    public void service() throws Exception {
-        // 클라이언트 연결을 기다리는 서버 소켓 준비
-        ServerSocket serverSocket = new ServerSocket(8888);
-        System.out.println("서버 실행 중...");
+    public void service()throws Exception{
         
-        while (true) {
-            try (
+        ServerSocket serverSocket = new ServerSocket(8888);
+        System.out.println("Server is running now...");
+        while(true) {
+            try(
                     Socket socket = serverSocket.accept();
                     PrintStream out = new PrintStream(
                             new BufferedOutputStream(
@@ -60,20 +61,20 @@ public class ServerApp {
                     BufferedReader in = new BufferedReader(
                             new InputStreamReader(
                                     socket.getInputStream()));
-                    ) {
+                    ){
                 System.out.println(in.readLine());
-                out.println("OK:강사"); out.flush();
+                out.println("AutumnHasReloaded"); out.flush();
                 
                 while (true) {
                     String requestLine = in.readLine();
-                    if (requestLine.equals("EXIT")) {
-                        out.println("goodbye");
+                    if(requestLine.equals("EXIT")) {
+                        out.println("AutumnHasOverloaded");
                         out.println();
                         out.flush();
                         break;
                     }
                     
-                    RequestMappingHandler mapping = 
+                    RequestMappingHandler mapping =
                             requestHandlerMap.getMapping(requestLine);
                     if (mapping == null) {
                         out.println("해당 요청을 처리할 수 없습니다.");
@@ -84,42 +85,22 @@ public class ServerApp {
                     
                     try {
                         mapping.getMethod().invoke(mapping.getInstance(), out);
+                        
                     } catch (Exception e) {
+                        System.out.println(e.getCause());
                         e.printStackTrace();
-                        out.println("요청 처리 중에 오류가 발생했습니다.");
+                        out.println("요청 처리중 오류가 발생했습니다!");
                     }
                     out.println();
                     out.flush();
                 }
-                
             }
         }
     }
     
     public static void main(String[] args) throws Exception {
-        ServerApp serverApp = new ServerApp();
+        ServerApp2 serverApp = new ServerApp2();
         serverApp.service();
+        
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
