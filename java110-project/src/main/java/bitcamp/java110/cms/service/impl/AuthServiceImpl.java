c5d1dc995c59075a6a066f7455ce1386129802d5
JeahaOh <jeaha1217@gmail.com>
2018-10-11 09:51:50 +0900

    Service Component 적용

---
+++ java110-project/src/main/java/bitcamp/java110/cms/service/impl/AuthServiceImpl.java
@@ -0,0 +1,40 @@
+package bitcamp.java110.cms.service.impl;
+
+import bitcamp.java110.cms.dao.ManagerDao;
+import bitcamp.java110.cms.dao.StudentDao;
+import bitcamp.java110.cms.dao.TeacherDao;
+import bitcamp.java110.cms.domain.Member;
+import bitcamp.java110.cms.service.AuthService;
+
+public class AuthServiceImpl implements AuthService {
+    
+    ManagerDao managerDao;
+    StudentDao studentDao;
+    TeacherDao teacherDao;
+
+    public void setStudentDao(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+    public void setTeacherDao(TeacherDao teacherDao) {
+        this.teacherDao = teacherDao;
+    }
+    public void setManagerDao(ManagerDao managerDao) {
+        this.managerDao = managerDao;
+    }
+    
+    @Override
+    public Member getMember(
+            String email, String password, String memberType) {
+        if (memberType.equals("manager")) {
+            return managerDao.findByEmailPassword(email, password);
+            
+        } else if (memberType.equals("student")) {
+            return studentDao.findByEmailPassword(email, password);
+            
+        } else if (memberType.equals("teacher")) {
+            return teacherDao.findByEmailPassword(email, password);
+        } else {
+            return null;
+        }
+    }
+}
